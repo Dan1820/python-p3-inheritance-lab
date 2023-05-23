c6a405a38debe1f6e01c30f63cd4775fbe257708
Duncan Kipkemoi <kipkemoiduncan7@gmail.com>
2023-05-23 12:25:26 +0300

    completed

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
 
 from user import User
+knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
+
 
 class Student(User):
-    
-    def learn(self):
-        pass
+
+    def learn(self, knowledge=""):
+        self.knowledge = knowledge
+        return self.knowledge
--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python
 
-from user import User
-
 import random
+from user import User
+knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
+
 
 class Teacher(User):
 
-    def teach(self):
-        pass
+    def teach(self, knowledge="str is a data type in Python"):
+
+        self.knowledge = knowledge
+        return self.knowledge
